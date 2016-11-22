84242588820f914cf2fca24647429ca9dc943f6d
Juan C <jccaicedo@gmail.com>
2016-11-22 02:38:13 +0000

    Progress with illumination correction and parallel computation

--- scripts/repurp/02IntensityStats.py
+++ scripts/repurp/02IntensityStats.py
@@ -9,24 +9,26 @@ import cPickle as pickle
 
 CHANNELS = ["RNA","ER","AGP","Mito","DNA"]
 
-def readMetadata(metaFile):
+def readMetadata(metaFile, i):
     metadata = meta.Metadata(metaFile)
     plates = metadata.data["Metadata_Plate"].unique()
     print "Total plates:",len(plates)
-    #plate = metadata.filterRecords(lambda df: (df.Metadata_Plate == plates[0]) & (df.Metadata_Well == "A01"), copy=True)
-    plate = metadata.filterRecords(lambda df: df.Metadata_Plate == plates[0], copy=True)
+    plate = metadata.filterRecords(lambda df: (df.Metadata_Plate == plates[i]) & (df.Metadata_Well == "A01"), copy=True)
+    #plate = metadata.filterRecords(lambda df: df.Metadata_Plate == plates[0], copy=True)
     ## TODO: Iterate over plates and yield each
     return plate
 
-def computeMaxIntensity(plate, root, outDir):
+def maxIntensity(args):
+    plate, root, outDir = args
     dataset = ds.Dataset(plate, "Treatment", CHANNELS, root)
-    hist = px.Histogram(16, 5)
+    hist = px.ImageStatistics(16, 5)
     hist.expected = dataset.numberOfRecords("all")
     dataset.scan(hist.processImage, frame="all")
     stats = hist.computeStats()
-    outfile = outDir + plate.data["Metadata_Plate"][0] + ".pkl"
+    outfile = outDir + plate.data["Metadata_Plate"].iloc[0] + ".pkl"
     with open(outfile,"wb") as output:
         pickle.dump(stats, output)
+    print 'Plate',plate.data["Metadata_Plate"].iloc[0],'done'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -35,5 +37,7 @@ if __name__ == "__main__":
     parser.add_argument("output_dir", help="Directory to store the statistics")
     args = parser.parse_args()
 
-    plate = readMetadata(args.metadata)
-    computeMaxIntensity(plate, args.root_dir, args.output_dir) 
+    plates = [ readMetadata(args.metadata, i) for i in range(4) ]
+    manager = utils.Parallel()
+    manager.compute(maxIntensity, plates, [args.root_dir, args.output_dir])
+
