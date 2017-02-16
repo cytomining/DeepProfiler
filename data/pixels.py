496ff4f27dc8114c5060d5a03bf66371dc5eec3a
Ubuntu <ubuntu@ip-10-0-5-243.ec2.internal>
2017-02-16 23:15:12 +0000

    Fixed bugs in illumination correction

--- data/pixels.py
+++ data/pixels.py
@@ -1,16 +1,17 @@
 import os
 import sys
 import argparse
-import utils
+import data.utils as utils
+import data.metadata as meta
 import skimage
 import skimage.io
 import skimage.transform
 import skimage.exposure
+import skimage.filters
+import skimage.morphology
 import scipy.misc
 import numpy as np
-import metadata as meta
-import dataset as ds
-import cPickle as pickle
+import pickle as pickle
 
 #################################################
 ## COMMON IMAGE HANDLING OPPERATIONS
@@ -44,32 +45,42 @@ class PixelProcessor():
 # Build pixel histogram for each channel
 class ImageStatistics():
 
-    def __init__(self, bits, channels, name=""):
+    def __init__(self, bits, channels, downScaleFactor, medianFilterSize, name=""):
         self.depth = 2**bits
-        #TODO: Change channels from number to the array with channel names
         self.channels = channels
         self.name = name
-        self.hist = np.zeros((channels, self.depth), dtype=np.float64)
+        self.downScaleFactor = downScaleFactor
+        self.medianFilterSize = medianFilterSize
+        self.hist = np.zeros((len(channels), self.depth), dtype=np.float64)
         self.count = 0
         self.expected = 1
         self.meanImage = None
+        self.originalImageSize = None
         
     def processImage(self, index, img, meta):
         self.addToMean(img)
         self.count += 1
         utils.logger.info("Plate {} Image {} of {} ({:4.2f}%)".format(self.name, 
                           self.count, self.expected, 100*float(self.count)/self.expected))
-        for i in range(self.channels):
+        for i in range(len(self.channels)):
             counts = np.histogram(img[:,:,i], bins=self.depth, range=(0,self.depth))[0]
             self.hist[i] += counts.astype(np.float64)
 
     # Accumulate the mean image. Useful for illumination correction purposes
     def addToMean(self, img):
-        # TODO: parameterize the target size
-        scale = 540
-        thumb = skimage.transform.resize(img, (scale,scale))
+        # Check image size (we assume all images have the same size)
+        if self.originalImageSize is None:
+            self.originalImageSize = img.shape
+            self.scale = (img.shape[0]/self.downScaleFactor, img.shape[1]/self.downScaleFactor)
+        else:
+            if img.shape != self.originalImageSize:
+                raise ValueError("Images in this plate don't match: required=",
+                                 self.originalImageSize, " found=", img.shape)
+        # Rescale original image to half
+        thumb = skimage.transform.resize(img, self.scale)
         if self.meanImage is None:
             self.meanImage = np.zeros_like(thumb, dtype=np.float64)
+        # Add image to current mean values
         self.meanImage += thumb.astype(np.float64)
         return
 
@@ -80,18 +91,28 @@ class ImageStatistics():
 
     # Compute global statistics on pixels. Useful for contrast/brightness adjustment (a.k.a histogram stretching)
     def computeStats(self):
+        # Initialize counters
         bins = np.linspace(0,self.depth-1,self.depth)
-        mean = np.zeros((self.channels))
-        lower = np.zeros((self.channels))
-        upper = np.zeros((self.channels))
+        mean = np.zeros((len(self.channels)))
+        lower = np.zeros((len(self.channels)))
+        upper = np.zeros((len(self.channels)))
         self.meanImage /= self.count
-        for i in range(self.channels):
+
+        # Compute percentiles and histogram
+        for i in range(len(self.channels)):
             probs = self.hist[i]/self.hist[i].sum()
             mean[i] = (bins * probs).sum()
             lower[i] = self.percentile(probs, 0.0001)
             upper[i] = self.percentile(probs, 0.9999)
-        # TODO: add channel names here, and also the original image size
-        stats = {'mean':mean, 'upper':upper, 'lower':lower, 'hist':self.hist, "MeanImg":self.meanImage}
+        stats = {"mean_values":mean, "upper_percentiles":upper, "lower_percentiles":lower, "histogram":self.hist, 
+                 "mean_image":self.meanImage, "channels":self.channels, "original_size":self.originalImageSize}
+
+        # Compute illumination correction function and add it to the dictionary
+        correct = IlluminationCorrection(stats, self.channels, self.originalImageSize)
+        correct.computeAll(self.medianFilterSize)
+        stats["illum_correction_function"] = correct.illumCorrFunc
+
+        # Plate ready
         utils.logger.info('Plate ' + self.name + ' done')
         return stats
 
@@ -106,27 +127,29 @@ class IlluminationCorrection():
     def __init__(self, stats, channels, targetDim):
         self.stats = stats
         self.channels = channels
-        self.targetDim = targetDim
+        self.targetDim = (targetDim[0], targetDim[1])
 
     # Based on the CellProfiler implementation of Illumination Correction
     # CellProfiler/cellprofiler/modules/correctilluminationcalculate.py
-    def computeFunction(self, meanChannel, diskSize=25):
+    def channelFunction(self, meanChannel, diskSize):
         #TODO: get np.type from other source or parameterize or compute :/
         # We currently assume 16 bit images
-        filteredChannel = median(meanChannel.astype(np.uint16), disk(diskSize))
+        operator = skimage.morphology.disk(diskSize)
+        filteredChannel = skimage.filters.median(meanChannel.astype(np.uint16), operator)
         filteredChannel = skimage.transform.resize(filteredChannel, self.targetDim)
         sortedPixels = filteredChannel[filteredChannel > 0]
         sortedPixels.sort()
-        idx = int(sortedPixels.shape[0] * ROBUST_FACTOR)
+        idx = int(sortedPixels.shape[0] * self.ROBUST_FACTOR)
         robustMinimum = sortedPixels[idx]
         filteredChannel[filteredChannel < robustMinimum] = robustMinimum
         illumCorrFunc = filteredChannel / robustMinimum
         return illumCorrFunc
 
-    def computeAll(self, diskSize=25):
+    def computeAll(self, medianFilterSize):
+        diskSize = medianFilterSize/2 # From diameter to radius
         illumCorrFunc = np.zeros( (self.targetDim[0], self.targetDim[1], len(self.channels)) )
         for ch in range(len(self.channels)):
-            illumCorrFunc[:,:,ch] = self.computeFunction(self.stats["MeanImg"][:,:,ch])
+            illumCorrFunc[:,:,ch] = self.channelFunction(self.stats["mean_image"][:,:,ch], diskSize)
         self.illumCorrFunc = illumCorrFunc
         return
 
@@ -149,15 +172,15 @@ class Compress():
 
     # Allows to recalculate the percentiles computed by default in the ImageStatistics class
     def recomputePercentile(self, p, side="upper"):
-        print "Percentiles for the",side," >> ",
+        print("Percentiles for the", side, " >> ", end='')
         self.stats[side] = np.zeros((len(self.channels)))
         for i in range(len(self.channels)):
-            probs = self.stats["hist"][i]/self.stats["hist"][i].sum()
+            probs = self.stats["histogram"][i]/self.stats["histogram"][i].sum()
             cum = np.cumsum(probs)
             pos = cum > p
             self.stats[side][i] = np.argmax(pos)
-            print self.channels[i],':',self.stats[side][i],
-        print ''
+            print(self.channels[i], ':', self.stats[side][i], end='')
+        print('')
 
     def setFormats(self, sourceFormat="tiff", targetFormat="png"):
         self.sourceFormat = sourceFormat
@@ -176,10 +199,10 @@ class Compress():
             # Downscale (TODO: Parameterize the resizing factor)
             image = skimage.transform.downscale_local_mean(img[:,:,c], factors=(2,2))
             # Stretch illumination values
-            image[image < self.stats["lower"][c]] = self.stats["lower"][c]
-            image[image > self.stats["upper"][c]] = self.stats["upper"][c]
+            image[image < self.stats["lower_percentiles"][c]] = self.stats["lower_percentiles"][c]
+            image[image > self.stats["upper_percentiles"][c]] = self.stats["upper_percentiles"][c]
             # TODO: add illumination correction here
-            # Save as PNG (TODO: save using ImageMagick)
+            # Save as PNG (#TODO: use ImageMagick for saving it to PNG-16bit)
             scipy.misc.imsave(self.targetPath(meta[self.channels[c]]), image)
         return
             
