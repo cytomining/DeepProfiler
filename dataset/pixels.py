Fixed validation bug, added single crop pixel normalization

dataset/pixels.py
@@ -14,8 +14,9 @@ def openImage(paths, outlines):
     for c in range(len(channels)):
         #TODO: dividing here is a terrible idea for illumination correction!
         #img[:,:,c] = (channels[c] -128.0) / 128.0
-        max_value = np.max(channels[c])
-        img[:,:,c] = channels[c] / max(max_value, 1.0)
+        #max_value =  max(np.max(channels[c]), 1.0)/2
+        #img[:,:,c] = (channels[c] - max_value) / max_value
+        img[:,:,c] = channels[c]
     if outlines is not None:
         boundaries = skimage.io.imread(outlines)
         labels = skimage.measure.label(boundaries, background=1)

learning/validation.py
@@ -110,6 +110,7 @@ class Validation(object):
                 output = [output]
             if self.save_features:
                 f = self.feat_extractor((batch[0], 0))
+                batch_size = batch[0].shape[0]
                 features[bp * batch_size:(bp + 1) * batch_size, :] = f[0]
 
             # Compute performance metrics for each target
@@ -124,7 +125,8 @@ class Validation(object):
         # Save features and report performance
         filebase = self.output_base(meta)
         if self.save_features:
-            features = features[:-batch_data["pads"], :]
+            if batch_data["pads"] > 0:
+                features = features[:-batch_data["pads"], :]
             np.savez_compressed(filebase + ".npz", f=features)
             print(filebase, features.shape)
 