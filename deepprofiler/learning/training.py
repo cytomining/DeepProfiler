1a25edc2f3b1eba7a8a913ad67f0a3b7da02423f
Santiago Benoit <santib27@gmail.com>
2018-07-06 16:49:52 -0400

    add random seeding

--- deepprofiler/learning/training.py
+++ deepprofiler/learning/training.py
@@ -13,15 +13,17 @@ import deepprofiler.imaging.cropping
 ## MAIN TRAINING ROUTINE
 #################################################
 
-def learn_model(config, dset, epoch):
+def learn_model(config, dset, epoch=1, seed=None):
 
-    model_module = importlib.import_module("plugins.models.{}".format(config['model']['model_name']))
-    crop_module = importlib.import_module("plugins.crop_generators.{}".format(config['model']['crop_generator_name']))
+    model_module = importlib.import_module("plugins.models.{}".format(config['model']['name']))
+    # crop_module = importlib.import_module("plugins.crop_generators.{}".format(config['model']['crop_generator']))
     importlib.invalidate_caches()
 
     model = model_module.define_model(config, dset)
-    crop_generator = crop_module.define_crop_generator(config, dset)
+    # crop_generator = crop_module.define_crop_generator(config, dset)
+    crop_generator = deepprofiler.imaging.cropping.CropGenerator(config, dset)
 
     dpmodel = deepprofiler.learning.model.DeepProfilerModel(model, config, dset, crop_generator)
-
+    if seed:
+        dpmodel.seed(seed)
     dpmodel.train(epoch)
