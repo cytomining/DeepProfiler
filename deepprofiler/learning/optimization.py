03e71438ec867aa2f0998b5763c2bb3a3616404f
Matthew Smith <msmith16.ma@gmail.com>
2018-07-26 09:26:13 -0400

    integrating suggestions made by Juan

--- deepprofiler/learning/optimization.py
+++ deepprofiler/learning/optimization.py
@@ -20,11 +20,19 @@ class Optimize(object):
         self.seed = seed
         self.bounds = []
         for i in range(len(self.config["optim"]["names"])):
-            self.bounds.append({
-                'name': self.config["optim"]["names"][i],
-                'type': self.config["optim"]["types"][i],
-                'domain': parse_tuple(self.config["optim"]["domains"][i])
-            })
+            if self.config["optim"]["names"][i] == "logarithmic":
+                self.bounds.append({
+                    'name': self.config["optim"]["names"][i],
+                    'type': "continuous",
+                    'domain': parse_tuple(self.config["optim"]["domains"][i])
+                })
+            else:
+                self.bounds.append({
+                    'name': self.config["optim"]["names"][i],
+                    'type': self.config["optim"]["types"][i],
+                    'domain': parse_tuple(self.config["optim"]["domains"][i])
+                })
+    
     
     def model(self):
         evaluation = deepprofiler.learning.training.learn_model(self.config, self.dset, self.epoch, self.seed, verbose=0)
@@ -33,9 +41,11 @@ class Optimize(object):
     def f(self, x):
         for i in range(len(self.config["optim"]["names"])):
             if self.config["optim"]["types"][i] == "continuous":
-                self.config['train']["model"]["params"][self.config["optim"]["names"][i]] = float(10**x[:,i])
+                self.config['train']["model"]["params"][self.config["optim"]["names"][i]] = float(x[:,i])
             elif self.config["optim"]["types"][i] == "discrete":
                 self.config['train']["model"]["params"][self.config["optim"]["names"][i]] = int(x[:,i])
+            elif self.config["optim"]["types"][i] == "logarithmic":
+                self.config['train']["model"]["params"][self.config["optim"]["names"][i]] = float(10**x[:,i])
         evaluation = self.model()
         return evaluation[0]
 
