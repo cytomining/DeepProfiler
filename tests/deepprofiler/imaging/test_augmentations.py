d53f69f88301380700388419be9c7ea0fbec9757
Juan C <jccaicedo@gmail.com>
2019-09-24 00:02:28 +0000

    Fixing augmentation test

--- tests/deepprofiler/imaging/test_augmentations.py
+++ tests/deepprofiler/imaging/test_augmentations.py
@@ -4,14 +4,27 @@ import tensorflow as tf
 
 import deepprofiler.imaging.augmentations
 
+config = tf.ConfigProto(
+    device_count = {'GPU': 0}
+)
+
 
 def test_augment():
-    crop = np.random.uniform(0, 1, (128, 128, 3)).astype(np.float32)
+    crop = tf.constant(
+        np.random.uniform(0, 1, (128, 128, 3)).astype(np.float32)
+    )
     augmented = deepprofiler.imaging.augmentations.augment(crop)
+    with tf.Session(config=config) as sess:
+        augmented = augmented.eval()
     assert augmented.shape == crop.shape
 
 
 def test_augment_multiple():
-    crops = np.random.uniform(0, 1, (10, 128, 128, 3)).astype(np.float32)
+    crops = tf.constant(
+        np.random.uniform(0, 1, (10, 128, 128, 3)).astype(np.float32)
+    )
     augmented = deepprofiler.imaging.augmentations.augment_multiple(crops)
+    with tf.Session(config=config) as sess:
+        augmented = augmented.eval()
+    print(augmented.shape, crops.shape)
     assert augmented.shape == crops.shape
