546997f37e9ab8629c0d00a6c5dc196c8e7f05cf
Allen Goodman <allen.goodman@icloud.com>
2019-10-22 16:01:15 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.1.0",
     author="Juan Caicedo",
     author_email="jcaicedo@gmail.com",
-    description=("Tools for representation learning in high throughput image collections"),
+    description="Tools for representation learning in high throughput image collections",
     license="BSD",
     keywords="",
     url="https://github.com/jccaicedo/DeepProfiler",
@@ -28,10 +28,15 @@ setuptools.setup(
         "pandas>=0.23.0",
         "scikit-image>=0.14.0",
         "scikit-learn>=0.19.0",
-        "scipy>=1.1",
-#        "tensorflow-gpu==1.12.2"
+        "scipy>=1.1"
     ],
     extras_require={
+        "cpu": [
+            "tensorflow==1.15.0"
+        ],
+        "gpu": [
+            "tensorflow-gpu==1.15.0"
+        ],
         "test": [
             "pytest>=3.6",
             "pytest-cov>=2.0",
