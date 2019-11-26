Fixing 360 rotation bug

deepprofiler/imaging/augmentations.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+PI = 3.141526539 
 #################################################
 ## CROPPING AND TRANSFORMATION OPERATIONS
 #################################################
@@ -21,7 +21,7 @@ def augment(crop):
         #augmented = tf.image.rot90(augmented, angle[0])
 
         # 360 degree rotations
-        angle = tf.random_uniform([1], minval=0.0, maxval=2*deepprofiler.dataset.utils.PI, dtype=tf.float32)
+        angle = tf.random_uniform([1], minval=0.0, maxval=2*PI, dtype=tf.float32)
         augmented = tf.contrib.image.rotate(augmented, angle[0], interpolation="BILINEAR")
 
         # Illumination changes