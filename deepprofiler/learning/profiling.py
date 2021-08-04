989121d0d35da7ba3663a468b0583c3d0b505fa0
Nikita Moshkov <nmoshkov@broadinstitute.org>
2021-08-04 16:31:14 -0400

    Import fix

--- deepprofiler/learning/profiling.py
+++ deepprofiler/learning/profiling.py
@@ -3,7 +3,7 @@ import os
 
 import numpy as np
 import tensorflow as tf
-from keras import backend as K
+from tensorflow.compat.v1.keras import backend as K
 
 from deepprofiler.dataset.utils import tic, toc
 
