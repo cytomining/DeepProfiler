f473ad4f4638f41279371b387274d3b1cd679c62
arkkienkeli <nikitam851@gmail.com>
2021-10-12 17:31:09 +0200

    Update augmentations.py with new class and augmentations

--- deepprofiler/imaging/augmentations.py
+++ deepprofiler/imaging/augmentations.py
@@ -1,31 +1,18 @@
-import numpy as np
 import tensorflow as tf
-import tensorflow_addons as tfa
-import sys
-
-tf.compat.v1.disable_v2_behavior()
 
 
 #################################################
 # CROPPING AND TRANSFORMATION OPERATIONS
 #################################################
 
-def random_crop(image):
-    w,h,c = image.shape
-    
-    size = tf.random.uniform([1], minval=int(w.value*0.6), maxval=w, dtype=tf.int32)
-    crop = tf.image.random_crop(image, [size[0],size[0],c])
-    
-    result = tf.image.resize(
-        tf.expand_dims(crop, 0), [w,h], method="bicubic"
-    )
-    
-    return result[0,...]
-
 
 def random_illumination(image):
     # Make channels independent images
-    numchn = image.shape[-1].value
+    if isinstance(image.shape[-1], int):
+        numchn = image.shape[-1]
+    else:
+        numchn = image.shape[-1].value
+
     source = tf.transpose(image, [2, 1, 0])
     source = tf.expand_dims(source, -1)
     source = tf.image.grayscale_to_rgb(source)
@@ -39,8 +26,7 @@ def random_illumination(image):
     
     # Recover multi-channel image
     result = tf.image.rgb_to_grayscale(result)
-    result = tf.transpose(result[:,:,:,0], [2, 1, 0])
-    #result = result / tf.math.reduce_max(result)
+    result = tf.transpose(result[:, :, :, 0], [2, 1, 0])
     return result
 
 
@@ -54,22 +40,24 @@ def random_flips(image):
     
     return augmented
 
-def random_rotate(image):
-    w, h, c = image.shape
-    image = tfa.image.rotate(image, np.pi / tf.random.uniform(shape=[], minval=1, maxval=10, dtype=tf.float32))
-    image = tf.image.central_crop(image, 0.7)
-    return tf.image.resize(image, (w, h))
+
+def random_crop(image):
+    if isinstance(image.shape[-1], int):
+        w, h, c = image.shape[0], image.shape[1], image.shape[-1]
+    else:
+        w, h, c = image.shape[0].value, image.shape[1].value, image.shape[-1].value
+    if tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32), tf.cast(0.5, tf.float32)):
+        size = tf.random.uniform([1], minval=int(w * 0.8), maxval=w, dtype=tf.int32)
+        image = tf.image.random_crop(image, [size[0], size[0], c])
+        return tf.image.resize(image, (w, h))
+    else:
+        return image
 
 
 def augment(image):
-    #if tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32), tf.cast(0.5, tf.float32)):
-    #    augm = random_crop(image)
-    #else:
-    #    augm = random_rotate(image)
-
-    augm = random_flips(image)
+    augm = random_crop(image)
+    augm = random_flips(augm)
     augm = random_illumination(augm)
-
     return augm
 
 
@@ -77,14 +65,27 @@ def augment_multiple(crops, parallel=None):
     return tf.map_fn(augment, crops, parallel_iterations=parallel, dtype=tf.float32)
 
 
-## A layer for GPU accelerated augmentations
-
-#AugmentationLayer = tf.keras.layers.Lambda(augment_multiple)
+# A layer for GPU accelerated augmentations
 
 class AugmentationLayer(tf.compat.v1.keras.layers.Layer):
     def __init__(self, **kwargs):
         self.is_training = True
         super(AugmentationLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        return
+
+    def call(self, input_tensor):
+        if self.is_training:
+            return augment_multiple(input_tensor)
+        else:
+            return input_tensor
+
+
+class AugmentationLayerV2(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        self.is_training = True
+        super(AugmentationLayerV2, self).__init__(**kwargs)
 
     def build(self, input_shape):
         return
