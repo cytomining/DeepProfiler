c09ba5277710a1faa739a3d0b3a8f53937041829
Gregory Way <gregory.way@gmail.com>
2026-06-16 07:41:11 -0600

    remove dedicated crop generator

--- tests/deepprofiler/crop_generators/test_crop_generator.py
+++
@@ -1,7 +0,0 @@
-import deepprofiler.crop_generators.crop_generator
-import deepprofiler.imaging.cropping
-
-
-def test_crop_generator():
-    assert deepprofiler.crop_generators.crop_generator.GeneratorClass == deepprofiler.imaging.cropping.CropGenerator
-    assert deepprofiler.crop_generators.crop_generator.SingleImageGeneratorClass == deepprofiler.imaging.cropping.SingleImageCropGenerator
